60624b5a617e6a4053b752ec88215be27ed22ec9
Byron Wang <byronwang123919@gmail.com>
2023-08-05 23:06:26 -0700

    updated image list

--- src/components/NotLoggedIn.js
+++ src/components/NotLoggedIn.js
@@ -1,20 +1,25 @@
-import { Box, Button } from "@chakra-ui/react";
+import { Box, Button, Img, Text } from "@chakra-ui/react";
 import React, { useContext, useEffect } from "react";
 import { SignedInContext } from "../App";
 import { auth, provider } from "../firebase/config";
 import { signInWithPopup } from "firebase/auth";
+import { addUser } from "../firebase/helpers";
+import { FaGoogle } from "react-icons/fa";
 
 const NotLoggedIn = () => {
-  const { value, setValue } = useContext(SignedInContext);
+  const { setValue } = useContext(SignedInContext);
 
-  const handleClick = () => {
-    signInWithPopup(auth, provider).then(({ user }) => {
-      console.log(user, " is the data");
-      const { uid, email, displayName, photoURL } = user;
-      setValue(uid);
+  const handleClick = async () => {
+    signInWithPopup(auth, provider)
+      .then(({ user }) => {
+        const { uid, email, displayName, photoURL } = user;
+        addUser(uid, displayName, email, photoURL);
 
-      localStorage.setItem("uid", uid);
-    });
+        setValue(uid);
+
+        localStorage.setItem("uid", uid);
+      })
+      .then(() => {});
   };
 
   useEffect(() => {
@@ -22,8 +27,35 @@ const NotLoggedIn = () => {
   }, []);
 
   return (
-    <Box>
-      <Button onClick={handleClick}>Click to sign in</Button>
+    <Box textAlign="-webkit-center" pb="70px">
+      <Img
+        maxWidth="1300px"
+        minWidth={{ base: "360px", sm: "500px", md: "600px" }}
+        boxSize="60%"
+        src={"./../register-page.png"}
+        alt="logo"
+      />
+      <Button
+        mt="30px"
+        size={{ base: "sm", sm: "sm", md: "md", lg: "lg" }}
+        onClick={handleClick}
+        bg="transparent"
+        border="1px solid white"
+        color="white"
+        _hover={{ bg: "#5A5A5A", color: "white" }}
+        _active={{ bg: "#5A5A5A", color: "white" }}
+        _focus={{ boxShadow: "none" }}
+        outline="none"
+      >
+        <Img bgColor="inherit" src={"./../google-logo-again.png"} boxSize={5} />
+        <Text
+          bgColor="inherit"
+          pl="8px"
+          fontSize={{ base: "sm", sm: "sm", md: "md", lg: "lg" }}
+        >
+          Continue with Google
+        </Text>
+      </Button>
     </Box>
   );
 };
