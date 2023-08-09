306ed4a07fac2cc2db6dcf792f1520580687ee3f
Byron Wang <byronwang123919@gmail.com>
2023-08-08 20:55:37 -0700

    initial logged in view

--- src/components/LoggedIn.js
+++ src/components/LoggedIn.js
@@ -1,14 +1,63 @@
-import { Box } from "@chakra-ui/react";
-import React from "react";
+import {
+  Avatar,
+  Box,
+  Button,
+  Img,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
+import React, { useContext, useEffect, useState } from "react";
+import { SignedInContext } from "../App";
+import { getUserData } from "../firebase/helpers";
+import Header from "./Header";
 
 const LoggedIn = () => {
+  const { value, setValue } = useContext(SignedInContext);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const [user, setUser] = useState(null);
+  const [loadingCheck, setLoadingCheck] = useState(false);
+
+  useEffect(() => {
+    console.log(value, " is VALUE FIRST");
+    console.log(loadingCheck, " is loadingCheck");
+    const getUserDetails = async () => {
+      const data = await getUserData(value);
+      return data;
+    };
+
+    const fetchUser = async () => {
+      const object = await getUserDetails();
+      console.log(object, " is OBJECT");
+      setUser(object);
+      setLoadingCheck(true);
+    };
+
+    fetchUser();
+    console.log("user is fetched");
+  }, [value, loadingCheck]);
+
+  useEffect(() => {
+    console.log(user, " is the user");
+    console.log(user?.photoURL, " is url");
+  }, [user]);
+
   const logout = () => {
     console.log("logging out!");
     localStorage.clear();
     window.location.reload();
   };
 
-  return <Box onClick={logout}>Log Out</Box>;
+  return (
+    <Box width="100%">
+      <Header />
+      <Box display="flex" flexDirection="row">
+        <Avatar boxSize="50px" src={user?.photoURL} alt="profile" />
+        <Text>Welcome {user?.firstName}</Text>
+      </Box>
+      <Button onClick={logout}>Log Out</Button>
+    </Box>
+  );
 };
 
 export default LoggedIn;
