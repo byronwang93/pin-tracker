1f8b621a6dc0bfe0f1f5b73a55a9387a201c89dd
Byron Wang <byronwang123919@gmail.com>
2023-08-08 20:56:10 -0700

    app work

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { Box, ChakraProvider, VStack } from "@chakra-ui/react";
 import React, { useState } from "react";
 import "./App.css";
+import Footer from "./components/Footer";
 import HomePage from "./components/HomePage";
 
 export const SignedInContext = React.createContext();
@@ -10,7 +11,17 @@ function App() {
   return (
     <ChakraProvider>
       <SignedInContext.Provider value={{ value, setValue }}>
-        <HomePage />
+        <VStack
+          display="flex"
+          w="100%"
+          minHeight="100vh"
+          justify={!value && "center"}
+        >
+          <HomePage />
+          {/* <Box pt="40px" position="relative" left="0" bottom="20px" right="0">
+            <Footer />
+          </Box> */}
+        </VStack>
       </SignedInContext.Provider>
     </ChakraProvider>
   );
