4c04bc893b2683f89a6fbeb315a3446efeb485e4
Byron Wang <byronwang123919@gmail.com>
2023-08-24 15:07:50 -0700

    refactoring 1

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,88 +1,13 @@
-import { Box, Flex, Text } from "@chakra-ui/react";
-import React, { useContext, useEffect, useState } from "react";
-import { SignedInContext } from "../App";
-import { gamesBowled, getHighestGameHand } from "../firebase/helpers";
+import { Box } from "@chakra-ui/react";
+import React from "react";
+import FirstStatsBox from "./FirstStatsBox";
+import SecondStatsBox from "./SecondStatsBox";
 
 const Stats = () => {
-  const { value, setValue } = useContext(SignedInContext);
-  const [totalGames, setTotalGames] = useState(0);
-  const [highestOne, setHighestOne] = useState(null);
-  const [highestTwo, setHighestTwo] = useState(null);
-
-  useEffect(() => {
-    const setData = async () => {
-      const games = await gamesBowled(value);
-      setTotalGames(games);
-      const highOne = await getHighestGameHand(value, 1);
-      setHighestOne(highOne);
-      const highTwo = await getHighestGameHand(value, 2);
-      setHighestTwo(highTwo);
-    };
-
-    setData();
-  }, []);
-
   return (
     <Box w="100%" textAlign="-webkit-center">
-      <Box
-        alignItems={{ base: "center", md: "flex-start" }}
-        display="flex"
-        flexDirection={{ base: "column", md: "row" }}
-        maxW="570px"
-        w={{ base: "230px", md: "70%" }}
-        pt="15px"
-        pb="15px"
-        justifyContent="center"
-        backgroundColor="#3C3D36"
-        borderRadius="10px"
-      >
-        <Box
-          ml={{ base: "55px", md: "20px" }}
-          alignSelf="baseline"
-          pr="10px"
-          mr="25px"
-          pl={{ base: "0px", md: "20px" }}
-          alignItems="baseline"
-          maxW="150px"
-          display="flex"
-          flexDirection="column"
-        >
-          <Text fontSize="18px" color="#A0A0A0">
-            Games Bowled
-          </Text>
-          <Text p="5px 0px 13px 0px" fontSize="30px">
-            {totalGames !== null ? totalGames : "--"}
-          </Text>
-        </Box>
-        <Box
-          alignSelf="baseline"
-          textAlign="left"
-          maxW="150px"
-          display="flex"
-          flexDirection="column"
-        >
-          <Text fontSize="18px" color="#A0A0A0">
-            Highest Game (one-handed)
-          </Text>
-          <Text p="5px 0px 13px 0px" fontSize="30px">
-            {highestOne !== null ? highestOne : "--"}
-          </Text>
-        </Box>
-        <Box
-          alignSelf="baseline"
-          maxW="150px"
-          textAlign="left"
-          display="flex"
-          flexDirection="column"
-        >
-          <Text fontSize="18px" color="#A0A0A0">
-            Highest Game (two-handed)
-          </Text>
-          <Text p="5px 0px 1px 0px" fontSize="30px">
-            {highestTwo !== null ? highestTwo : "--"}
-          </Text>
-        </Box>
-      </Box>
+      <FirstStatsBox />
+      <SecondStatsBox />
     </Box>
   );
 };
