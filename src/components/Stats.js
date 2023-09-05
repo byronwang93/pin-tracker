f34929a5a080fd8ca7ad7b9e41dfdf5c9b3ba8db
Byron Wang <byronwang123919@gmail.com>
2023-09-05 00:42:13 -0700

    charts is a desktop thing

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,15 +1,17 @@
-import { Box } from "@chakra-ui/react";
+import { Box, useBreakpointValue } from "@chakra-ui/react";
 import React from "react";
 import Charts from "./Charts";
 import FirstStatsBox from "./FirstStatsBox";
 import SecondStatsBox from "./SecondStatsBox";
 
 const Stats = () => {
+  const isDesktop = useBreakpointValue({ base: false, md: true });
+
   return (
     <Box w="100%" textAlign="-webkit-center">
       <FirstStatsBox />
       <SecondStatsBox />
-      <Charts />
+      {isDesktop && <Charts />}
     </Box>
   );
 };
