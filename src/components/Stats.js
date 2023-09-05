a6cdc69a45d08bde81082f83ffcc73249ab27b83
Byron Wang <byronwang123919@gmail.com>
2023-09-05 00:27:02 -0700

    added the charts

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,5 +1,6 @@
 import { Box } from "@chakra-ui/react";
 import React from "react";
+import Charts from "./Charts";
 import FirstStatsBox from "./FirstStatsBox";
 import SecondStatsBox from "./SecondStatsBox";
 
@@ -8,6 +9,7 @@ const Stats = () => {
     <Box w="100%" textAlign="-webkit-center">
       <FirstStatsBox />
       <SecondStatsBox />
+      <Charts />
     </Box>
   );
 };
