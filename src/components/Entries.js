676933c97f2e69b8235e43e21bf27e3e97e1aec5
Byron Wang <byronwang123919@gmail.com>
2023-09-07 12:10:21 -0700

    entries finalized UI

--- src/components/Entries.js
+++ src/components/Entries.js
@@ -1,8 +1,32 @@
-import { Box, HStack, Text } from "@chakra-ui/react";
-import React, { useState } from "react";
+import { Box, HStack, Img, Text, VStack } from "@chakra-ui/react";
+import React, { useContext, useEffect, useState } from "react";
+import { SignedInContext } from "../App";
+import { sortBowlsDate, sortBowlsScore } from "../firebase/helpers";
 
 const Entries = () => {
+  const { value } = useContext(SignedInContext);
   const [toggle, setToggle] = useState(0);
+  const [bowls, setBowls] = useState([]);
+
+  useEffect(() => {
+    const getBowls = async () => {
+      let temp = [];
+      if (toggle === 0) {
+        temp = await sortBowlsDate(value);
+      } else {
+        temp = await sortBowlsScore(value);
+      }
+
+      setBowls(temp);
+    };
+
+    getBowls();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [toggle]);
+
+  useEffect(() => {
+    console.log(bowls, " is the bowls");
+  }, [bowls]);
 
   const toggles = [
     { text: "Date", setting: 0 },
@@ -17,7 +41,7 @@ const Entries = () => {
       <Text pb="10px" color="#A0A0A0" fontSize="20px" textAlign="left">
         Sort by:
       </Text>
-      <HStack spacing="17px" textAlign="start">
+      <HStack spacing="17px" pb="15px" textAlign="start">
         {toggles.map(({ text, setting }, key) => {
           return (
             <Box
@@ -34,11 +58,49 @@ const Entries = () => {
                 setToggle(setting);
               }}
             >
-              <Text fontSize="23px">{text}</Text>
+              <Text fontSize="18px">{text}</Text>
             </Box>
           );
         })}
       </HStack>
+      <HStack pb="18px">
+        <Text fontSize="20px" w="126px" color="#A0A0A0">
+          score
+        </Text>
+        <Text fontSize="20px" pl="26px" pr="46px" color="#A0A0A0">
+          hand
+        </Text>
+        <Text fontSize="20px" color="#A0A0A0">
+          date
+        </Text>
+      </HStack>
+      <VStack maxHeight="450px" alignItems="baseline" overflowY="auto">
+        {bowls.map(({ score, throwStyle, date }, index) => {
+          return (
+            <HStack
+              p="12px"
+              borderRadius="7px"
+              w="100%"
+              bgColor={index % 2 === 0 && "#3C3D36"}
+            >
+              <Text w="90px" fontSize="20px">
+                {score}
+              </Text>
+              <Text w="120px" fontSize="20px">
+                {throwStyle}
+              </Text>
+              <Text w="150px" mr="80px" fontSize="20px">
+                {date}
+              </Text>
+              <HStack spacing="10px">
+                <Img boxSize={5} src={"./../view-more-icon.svg"} alt="logo" />
+                <Img boxSize={5} src={"./../edit-icon.svg"} alt="logo" />
+                <Img boxSize={5} src={"./../trash-icon.svg"} alt="logo" />
+              </HStack>
+            </HStack>
+          );
+        })}
+      </VStack>
     </Box>
   );
 };
